c0d895879d42896a2f767e4953374add84ef94e4
Alexander Lübeck <ndsvw@bitbucket.org>
2021-07-24 11:19:00 +0200

    Added syntax tests for cases when priorities are omitted.

--- FluentRandomPickerTests/GeneralSyntaxTests.cs
+++ FluentRandomPickerTests/GeneralSyntaxTests.cs
@@ -15,13 +15,11 @@ namespace FluentRandomPickerTests
             Out.Of().Value(8).AndValue(2).PickOne();
             Out.Of().Value(8).AndValue(2).AndValue(9).PickOne();
             Out.Of().Value(8).AndValue(2).AndValue(9).AndValue(5).AndValue(13).PickOne();
-            //Out.Of().Value(2).PickOne();
+
+            // Out.Of().Value(2).PickOne(); should not be possible
 
             Out.Of().Value(9).WithWeight(2).AndValue(12).WithWeight(3).PickOne();
             Out.Of().Value(9).WithWeight(2).AndValue(12).WithWeight(3).AndValue(5).WithWeight(5).PickOne();
-            //Out.Of().Value(1).AndValue(2).WithWeight(3).AndValue(4).WithWeight(5).PickOne();
-            //Out.Of().Value(1).WithWeight(2).AndValue(3).AndValue(4).WithWeight(5).PickOne();
-            //Out.Of().Value(1).WithWeight(2).AndValue(3).AndValue(4).WithWeight(5).AndValue(6).WithWeight(7).AndValue(8).PickOne();
 
             Out.Of().Value(10).WithPercentage(40).AndValue(20).WithPercentage(60).PickOne();
             Out.Of().Value(10).WithPercentage(40).AndValue(20).WithPercentage(30).AndValue(30).WithPercentage(30).PickOne();
@@ -41,6 +39,28 @@ namespace FluentRandomPickerTests
                 .Value(TimeSpan.FromMilliseconds(100))
                 .AndValue(TimeSpan.FromMinutes(2))
                 .AndValue(TimeSpan.FromHours(4));
+        }
+
+        [TestMethod]
+        public void SeparateTestsWithOmittedPriorities()
+        {
+            Out.Of().Value(8).AndValue(2).WithPercentage(2).PickOne();
+            Out.Of().Value(8).WithPercentage(2).AndValue(2).PickOne();
+            Out.Of().Value(8).WithPercentage(2).AndValue(2).AndValue(9).PickOne();
+            Out.Of().Value(8).AndValue(2).WithPercentage(2).AndValue(9).PickOne();
+            Out.Of().Value(8).AndValue(2).AndValue(9).WithPercentage(2).PickOne();
+            Out.Of().Value(8).WithPercentage(2).AndValue(2).WithPercentage(2).AndValue(9).PickOne();
+            Out.Of().Value(8).WithPercentage(2).AndValue(2).AndValue(9).WithPercentage(2).PickOne();
+            Out.Of().Value(8).AndValue(2).WithPercentage(2).AndValue(9).WithPercentage(2).PickOne();
+
+            Out.Of().Value(8).AndValue(2).WithWeight(2).PickOne();
+            Out.Of().Value(8).WithWeight(2).AndValue(2).PickOne();
+            Out.Of().Value(8).WithWeight(2).AndValue(2).AndValue(9).PickOne();
+            Out.Of().Value(8).AndValue(2).WithWeight(2).AndValue(9).PickOne();
+            Out.Of().Value(8).AndValue(2).AndValue(9).WithWeight(2).PickOne();
+            Out.Of().Value(8).WithWeight(2).AndValue(2).WithWeight(2).AndValue(9).PickOne();
+            Out.Of().Value(8).WithWeight(2).AndValue(2).AndValue(9).WithWeight(2).PickOne();
+            Out.Of().Value(8).AndValue(2).WithWeight(2).AndValue(9).WithWeight(2).PickOne();
         }
 
         [TestMethod]
