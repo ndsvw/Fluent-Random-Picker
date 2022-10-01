d703903285377b647918b22edfeb7593be6b2aef
Alexander Lübeck <ndsvw@gmx.de>
2022-10-01 08:31:49 +0200

    Improved doc of IRandomNumberGenerator methods

--- FluentRandomPicker/Random/IRandomNumberGenerator.cs
+++ FluentRandomPicker/Random/IRandomNumberGenerator.cs
@@ -6,20 +6,20 @@
     public interface IRandomNumberGenerator
     {
         /// <summary>
-        /// Generates a random int32 value.
+        /// Generates a random int32 value &gt;= 0 and &lt; Int32.MaxValue.
         /// </summary>
         /// <returns>A random int value.</returns>
         int NextInt();
 
         /// <summary>
-        /// Generates a random int32 value smaller than n.
+        /// Generates a random int32 value &gt;= 0 and &lt; n.
         /// </summary>
         /// <param name="n">The exclusive upper limit of the random int.</param>
         /// <returns>A random int value with the given limit.</returns>
         int NextInt(int n);
 
         /// <summary>
-        /// Generates a random int32 value smaller than n.
+        /// Generates a random int32 value &gt;= min and &lt; max
         /// </summary>
         /// <param name="min">The lower limit of the random int (inclusive).</param>
         /// <param name="max">The upper limit of the random int (exclusive).</param>
@@ -27,9 +27,9 @@
         int NextInt(int min, int max);
 
         /// <summary>
-        /// Generates a random double value larger or equal to 0.0 and smaller than 1.0.
+        /// Generates a random double value &gt;= 0.0 and &lt; 1.0.
         /// </summary>
-        /// <returns>A random double value larger or equal to 0.0 and smaller than 1.0.</returns>
+        /// <returns>A random double value &gt;= 0.0 and &lt; 1.0.</returns>
         double NextDouble();
     }
 }
