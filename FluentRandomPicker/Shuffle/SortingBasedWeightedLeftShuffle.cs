db3a8edce97945c3929f8383580da358d3955b06
Alexander Lübeck <ndsvw@gmx.de>
2022-10-08 10:39:11 +0200

    Prevented edge case in SortingBasedWeightedLeftShuffle Made sure that return value 0 of _rng.NextDouble() is not used.

--- FluentRandomPicker/Shuffle/SortingBasedWeightedLeftShuffle.cs
+++ FluentRandomPicker/Shuffle/SortingBasedWeightedLeftShuffle.cs
@@ -47,9 +47,21 @@ namespace FluentRandomPicker.Shuffle
             var n = elementsList.Count;
 
             return Enumerable.Range(0, n)
-                .OrderByDescending(k => Math.Pow(_rng.NextDouble(), 1.0 / elementsList[k].Priority))
+                .OrderByDescending(k => Math.Pow(GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive(), 1.0 / elementsList[k].Priority))
                 .Take(firstN)
                 .Select(i => elementsList[i]);
+        }
+
+        private double GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive()
+        {
+            // This conditional recursion is required to make sure, 0 is not returned.
+            // With random double = exactly 0, the priorities would not matter at all.
+            // Chances are low (like 1/(2^53)), but it's not impossible:
+            // https://stackoverflow.com/questions/73916767/what-are-the-chances-of-random-nextdouble-being-exactly-0
+            var randomDouble = _rng.NextDouble();
+            if (randomDouble != 0) // comparison without delta is intended
+                return randomDouble;
+            return GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive();
         }
     }
 }
