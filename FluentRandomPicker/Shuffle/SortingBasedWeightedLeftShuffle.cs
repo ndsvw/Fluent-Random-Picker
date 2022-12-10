e5856930aec90c5cc58b244641a6fab6630d9818
Alex <ndsvw@gmx.de>
2022-12-10 09:41:24 +0100

    Fixed #15 and wrote unit tests for it.

--- FluentRandomPicker/Shuffle/SortingBasedWeightedLeftShuffle.cs
+++ FluentRandomPicker/Shuffle/SortingBasedWeightedLeftShuffle.cs
@@ -15,6 +15,8 @@ namespace FluentRandomPicker.Shuffle
     {
         private readonly IRandomNumberGenerator _rng;
 
+        private static readonly IndexRankTupleComparer _indexRankTupleComparer = new();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SortingBasedWeightedLeftShuffle{T}"/> class.
         /// </summary>
@@ -46,10 +48,20 @@ namespace FluentRandomPicker.Shuffle
             var elementsList = elements.ToList();
             var n = elementsList.Count;
 
-            return Enumerable.Range(0, n)
-                .OrderByDescending(k => Math.Pow(GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive(), 1.0 / elementsList[k].Priority))
-                .Take(firstN)
-                .Select(i => elementsList[i]);
+            var ranks = Enumerable.Range(0, n)
+                            .Select(i => (Index: i, Rank: CalculateRank(elementsList[i].Priority)))
+                            .ToArray();
+
+            Array.Sort(ranks, _indexRankTupleComparer);
+
+            ImproveAccuracyIfNecessary(ranks, elementsList);
+
+            return ranks.Take(firstN).Select(x => elementsList[x.Index]);
+        }
+
+        private double CalculateRank(int priority)
+        {
+            return Math.Pow(GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive(), 1.0 / priority);
         }
 
         private double GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive()
@@ -59,9 +71,49 @@ namespace FluentRandomPicker.Shuffle
             // Chances are low (like 1/(2^53)), but it's not impossible:
             // https://stackoverflow.com/questions/73916767/what-are-the-chances-of-random-nextdouble-being-exactly-0
             var randomDouble = _rng.NextDouble();
-            if (randomDouble != 0) // comparison without delta is intended
-                return randomDouble;
-            return GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive();
+
+            // comparison without delta is intended
+            return randomDouble != 0 ? randomDouble : GenerateRandomDoubleBetween0ExclusiveAnd1Exclusive();
+        }
+
+        /// <summary>
+        /// Improves the accuracy of the ranks if necessary.
+        /// In rarer cases, multiple ranks can be exactly 1 although the priorities differ a lot:
+        /// https://github.com/ndsvw/Fluent-Random-Picker/issues/14#issuecomment-1264280921
+        /// This solves this problem.
+        /// </summary>
+        /// <param name="sortedRanks">The already generated ranks (sorted descending).</param>
+        /// <param name="elementsList">The elements.</param>
+        /// <param name="level">The accuracy level. The longer there are multiple ranks being exactly 1, the higher the level.</param>
+        internal void ImproveAccuracyIfNecessary((int Index, double Rank)[] sortedRanks, IList<ValuePriorityPair<T>> elementsList, int level = 2)
+        {
+            if (sortedRanks.Length == 0)
+                return;
+
+            int numberOf1Ranks = 0;
+
+            // comparison without delta is intended
+            while (sortedRanks[numberOf1Ranks].Rank == 1)
+            {
+                var index = sortedRanks[numberOf1Ranks].Index;
+                var newRank = level + CalculateRank(elementsList[index].Priority / level);
+                sortedRanks[numberOf1Ranks] = (index, newRank);
+                numberOf1Ranks++;
+            }
+
+            if (numberOf1Ranks >= 2)
+            {
+                Array.Sort(sortedRanks, 0, numberOf1Ranks, _indexRankTupleComparer);
+                ImproveAccuracyIfNecessary(sortedRanks, elementsList, level * 2);
+            }
+        }
+
+        private sealed class IndexRankTupleComparer : IComparer<(int Index, double Rank)>
+        {
+            public int Compare((int Index, double Rank) value1, (int Index, double Rank) value2)
+            {
+                return -value1.Rank.CompareTo(value2.Rank);
+            }
         }
     }
 }
--- FluentRandomPickerTests/Shuffle/SortingBasedWeightedLeftShuffleTests.cs
+++ FluentRandomPickerTests/Shuffle/SortingBasedWeightedLeftShuffleTests.cs
@@ -1,6 +1,9 @@
 ﻿using FluentRandomPicker.Random;
 using FluentRandomPicker.Shuffle;
+using FluentRandomPicker.ValuePriorities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 namespace FluentRandomPickerTests.Shuffle
 {
@@ -23,6 +26,179 @@ namespace FluentRandomPickerTests.Shuffle
             var shuffle = new SortingBasedWeightedLeftShuffle<int>(rng);
 
             Assert.That.AllNValuesCanChangePositions(shuffle);
+        }
+
+        [TestMethod]
+        public void ImproveAccuracyIfNecessary_MultipleRanksAre1_TheyAreHigherAfterwards()
+        {
+            var rng = new DefaultRandomNumberGenerator();
+            var shuffle = new SortingBasedWeightedLeftShuffle<char>(rng);
+
+            var ranks = new (int Index, double Rank)[]{
+                (0, 1),
+                (1, 1),
+                (2, 1),
+                (3, 1),
+                (4, 1),
+                (5, 0.5),
+            };
+
+            var elements = new List<ValuePriorityPair<char>>(){
+                new ValuePriorityPair<char>('a', 500),
+                new ValuePriorityPair<char>('b', 1000),
+                new ValuePriorityPair<char>('c', 1500),
+                new ValuePriorityPair<char>('d', 2000),
+                new ValuePriorityPair<char>('e', 2500),
+                new ValuePriorityPair<char>('f', 1),
+            };
+
+            shuffle.ImproveAccuracyIfNecessary(ranks, elements);
+
+            Assert.IsTrue(ranks[0].Rank > 1.99, "1st rank that was 1 needs to be higher now.");
+            Assert.IsTrue(ranks[1].Rank > 1.99, "2nd rank that was 1 needs to be higher now.");
+            Assert.IsTrue(ranks[2].Rank > 1.99, "3rd rank that was 1 needs to be higher now.");
+            Assert.IsTrue(ranks[3].Rank > 1.99, "4th rank that was 1 needs to be higher now.");
+            Assert.IsTrue(ranks[4].Rank > 1.99, "5th rank that was 1 needs to be higher now.");
+        }
+
+                [TestMethod]
+        public void ImproveAccuracyIfNecessary_MultipleRanksAre1_TheyAreAllOrderedAfterwards()
+        {
+            var rng = new DefaultRandomNumberGenerator();
+            var shuffle = new SortingBasedWeightedLeftShuffle<char>(rng);
+
+            var ranks = new (int Index, double Rank)[]{
+                (0, 1),
+                (1, 1),
+                (2, 1),
+                (3, 1),
+                (4, 1),
+                (5, 0.5),
+            };
+
+            var elements = new List<ValuePriorityPair<char>>(){
+                new ValuePriorityPair<char>('a', 500),
+                new ValuePriorityPair<char>('b', 1000),
+                new ValuePriorityPair<char>('c', 1500),
+                new ValuePriorityPair<char>('d', 2000),
+                new ValuePriorityPair<char>('e', 2500),
+                new ValuePriorityPair<char>('f', 1),
+            };
+
+            shuffle.ImproveAccuracyIfNecessary(ranks, elements);
+
+            Assert.IsTrue(ranks[0].Rank >= ranks[1].Rank, "1st rank must be >= 2nd rank.");
+            Assert.IsTrue(ranks[1].Rank >= ranks[2].Rank, "2nd rank must be >= 3rd rank.");
+            Assert.IsTrue(ranks[2].Rank >= ranks[3].Rank, "3rd rank must be >= 4th rank.");
+            Assert.IsTrue(ranks[3].Rank >= ranks[4].Rank, "4th rank must be >= 5th rank.");
+            Assert.IsTrue(ranks[4].Rank >= ranks[5].Rank, "5th rank must be >= 6th rank.");
+        }
+
+        [TestMethod]
+        public void ImproveAccuracyIfNecessary_MultipleValuesAre1ButOneIsLower_TheLowerValueDoesNotChange()
+        {
+            var rng = new DefaultRandomNumberGenerator();
+            var shuffle = new SortingBasedWeightedLeftShuffle<char>(rng);
+
+            var ranks = new (int Index, double Rank)[]{
+                (0, 1),
+                (1, 1),
+                (2, 1),
+                (3, 1),
+                (4, 1),
+                (5, 0.5),
+            };
+
+            var elements = new List<ValuePriorityPair<char>>(){
+                new ValuePriorityPair<char>('a', 500),
+                new ValuePriorityPair<char>('b', 1000),
+                new ValuePriorityPair<char>('c', 1500),
+                new ValuePriorityPair<char>('d', 2000),
+                new ValuePriorityPair<char>('e', 2500),
+                new ValuePriorityPair<char>('f', 1),
+            };
+
+            shuffle.ImproveAccuracyIfNecessary(ranks, elements);
+
+            Assert.IsTrue(ranks[5].Rank > 0.49 && ranks[5].Rank < 0.51, "The lower value must not change.");
+        }
+
+        [TestMethod]
+        public void ImproveAccuracyIfNecessary_MultipleValuesLowerThan1_TheyDoNotChange()
+        {
+            var rng = new DefaultRandomNumberGenerator();
+            var shuffle = new SortingBasedWeightedLeftShuffle<char>(rng);
+
+            var ranks = new (int Index, double Rank)[]{
+                (0, 0.99),
+                (1, 0.5),
+                (2, 0.01),
+            };
+
+            var elements = new List<ValuePriorityPair<char>>(){
+                new ValuePriorityPair<char>('a', 500),
+                new ValuePriorityPair<char>('b', 1000),
+                new ValuePriorityPair<char>('c', 1500),
+                new ValuePriorityPair<char>('d', 2000),
+                new ValuePriorityPair<char>('e', 2500),
+                new ValuePriorityPair<char>('f', 1),
+            };
+
+            shuffle.ImproveAccuracyIfNecessary(ranks, elements);
+
+            Assert.IsTrue(ranks[0].Rank > 0.98 && ranks[0].Rank < 1, "A value has changed.");
+            Assert.IsTrue(ranks[1].Rank > 0.49 && ranks[1].Rank < 0.51, "A value has changed.");
+            Assert.IsTrue(ranks[2].Rank > 0 && ranks[2].Rank < 0.02, "A value has changed.");
+        }
+
+        [TestMethod]
+        public void ImproveAccuracyIfNecessary_Multiple1RanksAndHighPriorities_TheyAreHigherAfterwards()
+        {
+            for(var i = 0; i < 10_000; i++){
+                var rng = new DefaultRandomNumberGenerator();
+                var shuffle = new SortingBasedWeightedLeftShuffle<char>(rng);
+
+                var ranks = new (int Index, double Rank)[]{
+                    (0, 1),
+                    (1, 1),
+                    (2, 1),
+                    (3, 1),
+                    (4, 1),
+                    (5, 0.5),
+                };
+
+                var elements = new List<ValuePriorityPair<char>>(){
+                    new ValuePriorityPair<char>('a', Int32.MaxValue),
+                    new ValuePriorityPair<char>('b', Int32.MaxValue),
+                    new ValuePriorityPair<char>('c', Int32.MaxValue),
+                    new ValuePriorityPair<char>('d', Int32.MaxValue / 2),
+                    new ValuePriorityPair<char>('e', Int32.MaxValue / 10),
+                    new ValuePriorityPair<char>('f', 1),
+                };
+
+                shuffle.ImproveAccuracyIfNecessary(ranks, elements);
+
+                Assert.IsTrue(ranks[0].Rank > 1.99, "1st rank that was 1 needs to be higher now.");
+                Assert.IsTrue(ranks[1].Rank > 1.99, "2nd rank that was 1 needs to be higher now.");
+                Assert.IsTrue(ranks[2].Rank > 1.99, "3rd rank that was 1 needs to be higher now.");
+                Assert.IsTrue(ranks[3].Rank > 1.99, "4th rank that was 1 needs to be higher now.");
+                Assert.IsTrue(ranks[4].Rank > 1.99, "5th rank that was 1 needs to be higher now.");
+            }
+        }
+
+        [TestMethod]
+        public void ImproveAccuracyIfNecessary_NoRanks_NothingHappens()
+        {
+            var rng = new DefaultRandomNumberGenerator();
+            var shuffle = new SortingBasedWeightedLeftShuffle<char>(rng);
+
+            var ranks = new (int Index, double Rank)[]{ };
+
+            var elements = new List<ValuePriorityPair<char>>(){ };
+
+            shuffle.ImproveAccuracyIfNecessary(ranks, elements);
+
+            Assert.AreEqual(0, ranks.Length);
         }
     }
 }
