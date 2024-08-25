a783462eb948ac7d8014f5e2c558f71f344fba5a
Alex <ndsvw@gmx.de>
2024-08-25 16:53:00 +0200

    Fixed typo

--- FluentRandomPicker/Shuffle/StochasticAcceptanceBasedWeightedLeftShuffle.cs
+++ FluentRandomPicker/Shuffle/StochasticAcceptanceBasedWeightedLeftShuffle.cs
@@ -10,7 +10,7 @@ namespace FluentRandomPicker.Shuffle;
 /// A shuffle algorithm that respects weights.
 /// This is a ~ linear run time implementation.
 /// More: https://www.researchgate.net/publication/51962025_Roulette-wheel_selection_via_stochastic_acceptance.
-/// Please not: The performance can be pretty bad if at least one weight is very high and others are very low
+/// Please note: The performance can be pretty bad if at least one weight is very high and others are very low
 ///             (e.g. [100.000.000, 1, 2, 3]).
 /// </summary>
 /// <typeparam name="T">The type of the values.</typeparam>
