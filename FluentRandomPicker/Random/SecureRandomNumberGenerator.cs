cd02a3c301fba00a7045428d0ffda26c9d643539
Alex <ndsvw@gmx.de>
2025-10-10 20:18:47 +0200

    Removed shared byte field in SecureRng class (for cases of concurrency) and made use of newer overloads that take Span<T>.

--- FluentRandomPicker/Random/SecureRandomNumberGenerator.cs
+++ FluentRandomPicker/Random/SecureRandomNumberGenerator.cs
@@ -9,14 +9,20 @@ namespace FluentRandomPicker.Random;
 /// </summary>
 public class SecureRandomNumberGenerator : IRandomNumberGenerator
 {
-    private readonly byte[] _tmpBytes = new byte[8];
     private readonly RandomNumberGenerator _secureRng = RandomNumberGenerator.Create();
 
     /// <inheritdoc/>
     public double NextDouble()
     {
-        _secureRng.GetBytes(_tmpBytes);
-        var bytesAsLong = BitConverter.ToInt64(_tmpBytes, 0);
+#if NETCOREAPP3_0_OR_GREATER
+        Span<byte> tmpBytes = stackalloc byte[8];
+        _secureRng.GetBytes(tmpBytes);
+        var bytesAsLong = BitConverter.ToInt64(tmpBytes);
+#else
+        var tmpBytes = new byte[8];
+        _secureRng.GetBytes(tmpBytes);
+        var bytesAsLong = BitConverter.ToInt64(tmpBytes, 0);
+#endif
         return (double)(bytesAsLong & long.MaxValue) / long.MaxValue;
     }
 
@@ -24,10 +30,11 @@ public class SecureRandomNumberGenerator : IRandomNumberGenerator
     public int NextInt()
     {
 #if NET5_0_OR_GREATER
-            return RandomNumberGenerator.GetInt32(int.MaxValue);
+        return RandomNumberGenerator.GetInt32(int.MaxValue);
 #else
-        _secureRng.GetBytes(_tmpBytes, 4, 4);
-        var bytesAsInt32 = BitConverter.ToInt32(_tmpBytes, 4);
+        var tmpBytes = new byte[8];
+        _secureRng.GetBytes(tmpBytes, 4, 4);
+        var bytesAsInt32 = BitConverter.ToInt32(tmpBytes, 4);
 
         // because Math.Abs(Int32.MinValue) throws exception.
         if (bytesAsInt32 == int.MinValue)
@@ -41,7 +48,7 @@ public class SecureRandomNumberGenerator : IRandomNumberGenerator
     public int NextInt(int n)
     {
 #if NET5_0_OR_GREATER
-            return RandomNumberGenerator.GetInt32(n);
+        return RandomNumberGenerator.GetInt32(n);
 #else
         return (int)(NextDouble() * n); // working with NextInt() + modulo instead casuses modulo bias!
 #endif
@@ -51,7 +58,7 @@ public class SecureRandomNumberGenerator : IRandomNumberGenerator
     public int NextInt(int min, int max)
     {
 #if NET5_0_OR_GREATER
-            return RandomNumberGenerator.GetInt32(min, max);
+        return RandomNumberGenerator.GetInt32(min, max);
 #else
         return min + NextInt(max - min);
 #endif
